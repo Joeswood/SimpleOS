9b241bf90c5f4cd079d9263511b59b74cb748140
1012917602@qq.com <1012917602@qq.com>
2023-09-21 09:10:00 -0400

    Add common type header file

---
+++ source/comm/types.h
@@ -0,0 +1,24 @@
+/**
+ * Basic data type in system
+ */
+#ifndef TYPES_H
+#define TYPES_H
+
+// for avoiding conflict with newlib data type, add _UINT8_T_DECLARED
+#ifndef _UINT8_T_DECLARED
+#define _UINT8_T_DECLARED
+typedef unsigned char uint8_t;
+#endif
+
+#ifndef _UINT16_T_DECLARED
+#define _UINT16_T_DECLARED
+typedef unsigned short uint16_t;
+#endif
+
+#ifndef _UINT32_T_DECLARED
+#define _UINT32_T_DECLARED
+typedef unsigned long uint32_t;
+#endif
+
+#endif
+
