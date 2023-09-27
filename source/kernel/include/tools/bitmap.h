79144f02931e17cc20140bc46f7dfc2e9cf4ee7a
1012917602@qq.com <1012917602@qq.com>
2023-09-27 09:10:00 -0400

    Add bitmap structure

---
+++ source/kernel/include/tools/bitmap.h
@@ -0,0 +1,25 @@
+/**
+ * Bitmap
+ */
+#ifndef BITMAP_H
+#define BITMAP_H
+
+#include "comm/types.h"
+
+/**
+ * @brief Bitmap structure
+ */
+typedef struct _bitmap_g {
+    int bit_count;              // Bit data
+    uint8_t * bits;             // bit space
+}bitmap_t;
+
+void bitmap_init (bitmap_t * bitmap, uint8_t * bits, int count, int init_bit);
+int bitmap_byte_count (int bit_count);
+int bitmap_get_bit (bitmap_t * bitmap, int index);
+void bitmap_set_bit (bitmap_t * bitmap, int index, int count, int bit);
+int bitmap_is_set (bitmap_t * bitmap, int index);
+int bitmap_alloc_nbits (bitmap_t * bitmap, int bit, int count);
+
+#endif // BITMAP_H
+
